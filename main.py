002f35f1629b6637a53cf92542c62bc35c7f5039
asuplick-lang <asuplick@udel.edu>
2025-12-06 15:33:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ set_site_information(
     author=["asuplick@udel.edu", "benbaker@udel.edu"],
     description="""Helps users with disabilities use internet tools
     more accessibly.""",
+    sources = ["Drafter documentation"],
+    planning = ["IMG_2747.jpeg"]
     links=["https://github.com/UD-F25-CS1/honors-hackathon-2025-redding-5b"]
 )
 set_website_style("none")
